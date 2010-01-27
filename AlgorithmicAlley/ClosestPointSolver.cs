a3aecb13528cb886ed0e85a2875da799ba6a2bb6
Ben Fulton <benmarkfulton@gmail.com>
2010-01-26 21:28:52 -0500

    Initial commit and brute-force solution to the closest-pair problem.

---
+++ AlgorithmicAlley/ClosestPointSolver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace AlgorithmicAlley
+{
+    public class Segment
+    {
+        public Segment(PointF p1, PointF p2)
+        {
+            P1 = p1;
+            P2 = p2;
+        }
+
+        public readonly PointF P1;
+        public readonly PointF P2;
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(LengthSquared());
+        }
+
+        public float LengthSquared()
+        {
+            return (P1.X - P2.X) * (P1.X - P2.X)
+                + (P1.Y - P2.Y) * (P1.Y - P2.Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Segment;
+            if (other == null) return false;
+
+            return P1 == other.P1 && P2 == other.P2;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", P1, P2);
+        } 
+    }
+
+    public class ClosestPointSolver
+    {
+        public Segment Closest_BruteForce(List<PointF> points)
+        {
+            int n = points.Count;
+            var allPairs = Enumerable.Range(0, n - 1)
+                .SelectMany(i => Enumerable.Range(i + 1, n - (i + 1))
+                    .Select(j => new Segment(points[i], points[j])));
+
+            return allPairs.OrderBy(seg => seg.LengthSquared())
+                    .First();
+        }
+    }
+}
---
+++ AlgorithmicAlleyTests/ClosestPointTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Drawing;
+using AlgorithmicAlley;
+
+namespace AlgorithmicAlleyTests
+{
+    public class ClosestPointTests
+    {
+        IEnumerable<PointF> CreatePoints(int count)
+        {
+            var randomizer = new Random(10);
+            var points = Enumerable.Range(0, count)
+                .Select( i => new PointF( (float)randomizer.NextDouble(), (float)randomizer.NextDouble())).ToList();
+
+            return points;
+        }
+
+        [Fact]
+        public void A_list_with_two_points_returns_them_as_closest()
+        {
+            var points = CreatePoints(2).ToList();
+            Segment closest = new ClosestPointSolver().Closest_BruteForce(points);
+            Assert.Equal(closest.P1, points[0]);
+            Assert.Equal(closest.P2, points[1]);
+        }
+
+        [Fact]
+        public void a_list_with_1000_points_finds_the_closest()
+        {
+            var points = CreatePoints(1000).ToList();
+            var expected = new Segment(new PointF(0.7874735f, 0.9796776f), new PointF(0.786889f, 0.980248451f));
+            Segment closest = new ClosestPointSolver().Closest_BruteForce(points);
+            Assert.Equal(expected, closest);
+        }
+    }
+}
