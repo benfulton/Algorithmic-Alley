421d3a44ce7edb6b40351c8b7e24dcad30dcbde1
Ben Fulton <benmarkfulton@gmail.com>
2010-05-03 21:35:10 -0400

    Methods and tests for scheduling algorithms

---
+++ AlgorithmicAlley/Scheduler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmicAlley
+{
+    public class Scheduler
+    {
+        public List<int> SubsetFor(List<int> list, int sum)
+        {
+            // stolen from http://igoro.com/archive/7-tricks-to-simplify-your-programs-with-linq/
+            // only works on lists with length of 30 or less
+             var subsets = from m in Enumerable.Range(0, 1 << list.Count)
+              select
+                  from i in Enumerable.Range(0, list.Count)
+                  where (m & (1 << i)) != 0
+                  select list[i];
+
+             return subsets.First(set => set.Sum() == sum).ToList();
+        }
+
+        public List<int> GreedySubsetFor(List<int> list, int sum)
+        {
+            var result = new List<int>();
+            foreach (int i in list.OrderByDescending(k => k).SkipWhile( k => k > sum))
+            {
+                if (result.Sum() + i > sum)
+                    return result;
+                else
+                    result.Add(i);
+            }
+
+            return result;
+            
+        }
+        IEnumerable<IEnumerable<int>> RemoveCloseSums(List<IEnumerable<int>> list, float c)
+        {
+            var y = list.First();
+            yield return y;
+
+            foreach (var i in list)
+            {
+                if (y.Sum() < (1 - c / list.Count) * i.Sum())
+                {
+                    y = i;
+                    yield return i;
+                }
+            }
+        }
+
+        public List<int> ApproximateSubsetFor( List<int> list, int sum, float c)
+        {
+            var subsets = new List<IEnumerable<int>> { new List<int>() };
+            for (int i = 0; i < list.Count; i++)
+            {
+            	var T = list.SelectMany( j => subsets.Select( k => k.Union(new[]{j})));
+                var U = subsets.Union(T).OrderBy( k => k.Sum() ).ToList();
+
+                subsets = RemoveCloseSums(U, c).ToList();
+                var result = subsets.Find(s => s.Sum() <= sum && s.Sum() > (1 - c) * sum);
+                if (result != null)
+                    return result.ToList();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ AlgorithmicAlleyTests/SubsetSumTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using AlgorithmicAlley;
+
+namespace AlgorithmicAlleyTests
+{
+    class SubsetSumTests
+    {
+
+        [Fact]
+        public void A_subset_of_one_and_two_sums_to_one()
+        {
+            var list = new List<int> { 1, 2 };
+            Assert.Equal( 2, new Scheduler().SubsetFor(list, 2).Sum());
+        }
+
+        [Fact]
+        public void Can_find_subset_of_14_from_5_elements()
+        {
+            var list = new List<int> { 1, 2,3,4,10 };
+            Assert.Equal( 14, new Scheduler().SubsetFor(list, 14).Sum());
+        }
+
+        [Fact]
+        public void Can_find_subset_of_668_from_10_elements()
+        {
+            var list = new List<int> { 1, 2, 92, 437, 16, 46, 991, 12, 92, 130 };
+            Assert.Equal(668, new Scheduler().SubsetFor(list, 668).Sum());
+        }
+
+        [Fact]
+        public void Greedy_can_find_approximate_subset_of_668_from_10_elements()
+        {
+            var list = new List<int> { 1, 2, 92, 437, 16, 46, 991, 12, 92, 130 };
+            float delta = 0.02f;
+            var result = new Scheduler().GreedySubsetFor(list, 668);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+        [Fact]
+        public void Can_find_approximate_subset_of_668_from_10_elements()
+        {
+            var list = new List<int> { 1, 2, 92, 437, 16, 46, 991, 12, 92, 130 };
+            float delta = 0.01f;
+            List<int> result = new Scheduler().ApproximateSubsetFor(list, 668, delta);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+        [Fact]
+        public void Greedy_can_find_subset_of_668_from_24_random_elements()
+        {
+            var randomizer = new Random(10);
+            var list = Enumerable.Range(0, 24).Select(i => randomizer.Next(100)).ToList();
+            float delta = 0.1f;
+            var result = new Scheduler().GreedySubsetFor(list, 668);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+        [Fact]
+        public void Can_find_approximate_subset_of_668_from_24_random_elements()
+        {
+            var randomizer = new Random(10);
+            var list = Enumerable.Range(0, 24).Select(i => randomizer.Next(100)).ToList();
+            float delta = 0.01f;
+            List<int> result = new Scheduler().ApproximateSubsetFor(list, 668, delta);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+        [Fact]
+        public void Can_find_exact_subset_of_668_from_24_random_elements()
+        {
+            var randomizer = new Random(10);
+            var list = Enumerable.Range(0, 24).Select(i => randomizer.Next(100)).ToList();
+            float delta = 0.01f;
+            Assert.Throws<OutOfMemoryException>( () => { new Scheduler().SubsetFor(list, 668); });
+        }
+
+        [Fact]
+        public void Greedy_can_find_approximate_subset_of_668_from_1000_random_elements()
+        {
+            var randomizer = new Random(10);
+            var list = Enumerable.Range(0, 1000).Select(i => randomizer.Next(100)).ToList();
+            float delta = 0.2f;
+            var result = new Scheduler().GreedySubsetFor(list, 668);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+        [Fact]
+        public void Can_find_approximate_subset_of_668_from_1000_random_elements()
+        {
+            var randomizer = new Random(10);
+            var list = Enumerable.Range(0, 1000).Select(i => randomizer.Next(100)).ToList();
+            float delta = 0.01f;
+            List<int> result = new Scheduler().ApproximateSubsetFor(list, 668, delta);
+            Assert.InRange(result.Sum(), 668 * (1 - delta), 668 * (1 + delta));
+        }
+
+    }
+}
