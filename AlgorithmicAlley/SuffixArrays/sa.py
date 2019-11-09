0143a8e2e61d7830c943423c4241dd5d30993010
NavyaZaveri <navyajayesh+1@gmail.com>
2019-11-09 22:36:56 +0000

    consistent spacing

--- AlgorithmicAlley/SuffixArrays/sa.py
+++ AlgorithmicAlley/SuffixArrays/sa.py
@@ -1,14 +1,11 @@
 import time
 from collections import defaultdict, Counter
 
-
 def get_suffix_array(str):
     return sorted(range(len(str)), key=lambda i: str[i:])
 
-
 def suffix_array_ManberMyers(str):
     result = []
-
     def sort_bucket(str, bucket, order=1):
         d = defaultdict(list)
         for i in bucket:
@@ -21,13 +18,12 @@ def suffix_array_ManberMyers(str):
                 result.append(v[0])
         return result
 
-    return sort_bucket(str, (i for i in range(len(str))))
-
+    return sort_bucket(str, (i for i in range(len(str))))	    
 
 if __name__ == "__main__":
     with open("..\MobyDick.txt") as f:
         m = f.read()
-    str = m  # [:100000]
+    str = m#[:100000]
     print(len(str))
 #    str = "mississipi"
     start_time = time.time()
